833155e31b0a584c59acc964b989bd01faa139f1
GustavoRodrigueess <gustavorodrigueslima2004@gmail.com>
2022-11-13 16:14:28 -0300

    2SEM orientacao a objetos

---
+++ orientacao_a_obj/aula 8 - Encapsulamento/encapsulamento.md
@@ -0,0 +1,7 @@
+# Encapsulamento
+> O que e?
+
+- Basicamente é o desejo de querer esconder um método do uso de clientes. Então sempre que um método não deve ser reutilizado por outros desenvolvedores. Há uma indicação antes do método marcado por dois underscores -> "__metodo()" 
+> Entretanto, ainda assim consegue ser chamado, sem problema algum. 
+
+Mesta mesma pasta segue um código como exemplo de encapsulamento
---
+++ orientacao_a_obj/aula 8 - Encapsulamento/encapsulamento.py
@@ -0,0 +1,13 @@
+class Funcionario:
+    def __init__(self, nome, cargo, valor_hora_trabalhada):
+        self.nome = nome
+        self.cargo = cargo
+        self.valor_hora_trabalhada = valor_hora_trabalhada
+        self.__horas_trabalhadas = 0
+        self.__salario = 0
+
+    def registra_hora_trabalhada(self):
+        self.horas_trabalhadas += 1
+
+    def calcula_salario(self):
+        self.__salario = self.__horas_trabalhadas * self.valor_hora_trabalhada
