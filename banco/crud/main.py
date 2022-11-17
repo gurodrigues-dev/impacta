840f2f3b926119962e12518dc6aa0d7a64003536
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2022-11-17 16:27:39 -0300

    2SEM banco de dados

--- banco/crud/main.py
+++
@@ -1,8 +0,0 @@
-import logging
-import mysql.connector
-
-cnx = mysql.connector.connect(user='root', password='mysql',
-                              host='127.0.0.1',
-                              port = 3306, 
-                              database='bancoteste')
-cnx.close()
