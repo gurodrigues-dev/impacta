43e337168fde9cb7748fe66eac4fcf7ea6d9bb69
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2022-11-17 16:26:20 -0300

    2SEM banco de dados

--- banco/crud/main.py
+++ banco/crud/main.py
@@ -1,25 +1,8 @@
 import logging
 import mysql.connector
-from mysql.connector import errorcode
 
-
-try:
-    
-    db_connection = mysql.connector.connect(host='localhost', user='root', password='mysql', database='default')
-
-    print("Conexão feita com sucesso")
-
-exception Exception as err:
-    if error.errno == errorcode.ER_BAD_DB_ERROR:
-
-        print("Banco de Dados não existe")
-
-    elif error.errno == errprcpde.ER_ACESS_DENIED_ERROR:
-
-        print("Email ou senha incorretos")
-
-    else:
-
-        print(err)
-
-
+cnx = mysql.connector.connect(user='root', password='mysql',
+                              host='127.0.0.1',
+                              port = 3306, 
+                              database='bancoteste')
+cnx.close()
