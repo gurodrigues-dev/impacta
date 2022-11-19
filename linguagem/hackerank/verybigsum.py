7f8313e4248bd4e22e8caa567d8a5232fbee6625
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2022-11-19 18:44:46 -0300

    1SEM linguagem de programacao

--- linguagem/hackerank/verybigsum.py
+++ linguagem/hackerank/verybigsum.py
@@ -1,13 +1,34 @@
 try:
-    count = 0
-    
     number = int(input())
-    array = list(map(int, input().rstrip().split(" ")))
+    matriz = []
+    count_primaria = -1
+    count_secundaria = -1
+    primaria = 0
+    secundaria = 0
     
-    for item in array:
-        count += item
-        
-    print(count)
+    for i in range(number):
+        array = list(map(int, input().rstrip().split(" ")))
+        matriz.append(array)
 
+    # calculo da matriz principal 
+    for item in matriz:
+        count_primaria += 1
+        primaria += item[count_primaria]
+
+    # calculo da matriz secundaria
+    matriz.reverse()
+    
+    for item in matriz:
+        count_secundaria += 1
+        secundaria += item[count_secundaria]
+
+    if primaria > secundaria:
+        resultado = primaria - secundaria
+        print(resultado)
+    else:
+        resultado = secundaria - primaria
+        print(resultado)
+    
 except Exception as err:
+
     print(err)
