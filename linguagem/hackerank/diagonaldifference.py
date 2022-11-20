f1845958b16bde2cf152a86cb19ba75395b05e5d
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2022-11-20 19:46:38 -0300

    1SEM linguagem de programação

--- linguagem/hackerank/diagonaldifference.py
+++ linguagem/hackerank/diagonaldifference.py
@@ -25,3 +25,37 @@ try:
 except Exception as err:
     print(err)
 
+try:
+    number = int(input())
+    matriz = []
+    count_primaria = -1
+    count_secundaria = -1
+    primaria = 0
+    secundaria = 0
+    
+    for i in range(number):
+        array = list(map(int, input().rstrip().split(" ")))
+        matriz.append(array)
+
+    # calculo da matriz principal 
+    for item in matriz:
+        count_primaria += 1
+        primaria += item[count_primaria]
+
+    # calculo da matriz secundaria
+    matriz.reverse()
+    
+    for item in matriz:
+        count_secundaria += 1
+        secundaria += item[count_secundaria]
+
+    if primaria > secundaria:
+        resultado = primaria - secundaria
+        print(resultado)
+    else:
+        resultado = secundaria - primaria
+        print(resultado)
+    
+except Exception as err:
+
+    print(err)
--- linguagem/hackerank/verybigsum.py
+++ linguagem/hackerank/verybigsum.py
@@ -1,34 +1,13 @@
 try:
+    count = 0
+    
     number = int(input())
-    matriz = []
-    count_primaria = -1
-    count_secundaria = -1
-    primaria = 0
-    secundaria = 0
+    array = list(map(int, input().rstrip().split(" ")))
     
-    for i in range(number):
-        array = list(map(int, input().rstrip().split(" ")))
-        matriz.append(array)
+    for item in array:
+        count += item
+        
+    print(count)
 
-    # calculo da matriz principal 
-    for item in matriz:
-        count_primaria += 1
-        primaria += item[count_primaria]
-
-    # calculo da matriz secundaria
-    matriz.reverse()
-    
-    for item in matriz:
-        count_secundaria += 1
-        secundaria += item[count_secundaria]
-
-    if primaria > secundaria:
-        resultado = primaria - secundaria
-        print(resultado)
-    else:
-        resultado = secundaria - primaria
-        print(resultado)
-    
 except Exception as err:
-
     print(err)
